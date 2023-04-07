9d7ab3f7c74ca17e88f572e12ac430a753f996e4
n-rincon4 <n.rincon4@uniandes.edu.co>
2023-04-07 03:58:01 -0500

    Funcionamiento teleoperado listo

---
+++ build/mi_robot_15/prefix_override/sitecustomize.py
@@ -0,0 +1,3 @@
+import sys
+sys.real_prefix = sys.prefix
+sys.prefix = sys.exec_prefix = '/home/robotica/taller2_grupo15/install/mi_robot_15'
---
+++ robot_effector/motoresRobot/motoresRobot.ino
@@ -0,0 +1,150 @@
+#include <string.h>
+
+// Velocidades
+float velLineal;
+float velAngular;
+
+// Motor A
+int enA = 52;
+int in1 = 50;
+int in2 = 48;
+ 
+// Motor B
+int enB = 22;
+int in3 = 24;
+int in4 = 26;
+
+void setup()
+{
+  Serial.begin(9600);
+  // Terminales de salida en el Arduino
+  pinMode(enA, OUTPUT);
+  pinMode(enB, OUTPUT);
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(in3, OUTPUT);
+  pinMode(in4, OUTPUT);
+}
+
+void loop()
+{
+  if (Serial.available()) {
+    delay(10);
+    // Read input from serial
+    String data = Serial.readStringUntil('\n');
+    int separator = data.indexOf(',');
+    String value1String = data.substring(0, separator);
+    String value2String = data.substring(separator + 1);
+    // Extract linear and angular velocities from input string
+    float velLineal = value1String.toFloat();
+    float velAngular = value2String.toFloat();
+  
+  
+    if (velLineal > 0.0 && velAngular == 0.0) 
+    {
+      adelante(velLineal);
+    }
+    else if (velLineal < 0.0 && velAngular == 0.0) 
+    {
+      atras(velLineal);
+    }
+    else if (velLineal == 0.0 && velAngular > 0.0)
+    {
+      izquierda(velAngular);
+    }
+    else if (velLineal == 0.0 && velAngular < 0.0)
+    {
+      derecha(velAngular);
+    }
+    else {
+      quieto();
+    }
+  }
+}
+
+void adelante(float velocidadLineal)
+{
+  // Calcular la velocidad respecto al máximo (12cm/s)
+  int activacion = int(abs(255*(velocidadLineal/12)));
+
+  // Velocidad del Motor A y B
+  analogWrite(enA, activacion);
+  analogWrite(enB, activacion);
+
+  // Motor A gira adelante
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+
+  // Motor B gira adelante
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
+}
+
+void atras(float velocidadLineal)
+{
+  // Calcular la velocidad respecto al máximo (12cm/s)
+  int activacion = int(abs(255*(velocidadLineal/12)));
+
+  // Velocidad del Motor A y B
+  analogWrite(enA, activacion);
+  analogWrite(enB, activacion);
+
+  // Motor A gira adelante
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+
+  // Motor B gira adelante
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, HIGH);
+}
+
+void izquierda(float velocidadAngular)
+{
+  // Calcular la velocidad respecto al máximo (45deg/s)
+  int activacion = int(abs(255*(velocidadAngular/45)));
+
+  // Velocidad del Motor A y B
+  analogWrite(enA, activacion);
+  analogWrite(enB, 0);
+
+  // Motor A gira adelante
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+
+  // Motor B queda estático
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, LOW);
+}
+
+void derecha(float velocidadAngular)
+{
+  // Calcular la velocidad respecto al máximo (45deg/s)
+  int activacion = int(abs(255*(velocidadAngular/45)));
+
+  // Velocidad del Motor A y B
+  analogWrite(enA, 0);
+  analogWrite(enB, activacion);
+
+  // Motor A queda estático
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
+
+  // Motor B gira adelante
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
+}
+
+void quieto()
+{
+  // Velocidad del Motor A y B
+  analogWrite(enA, 0);
+  analogWrite(enB, 0);
+
+  // Motor A gira adelante
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
+
+  // Motor B queda estático
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, LOW);
+}
